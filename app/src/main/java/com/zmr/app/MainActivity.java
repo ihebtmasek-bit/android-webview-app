d390e02b21c23350a40981def4ddf35351eedec4
ihebtmasek-bit <ihebtmasek@gmail.com>
2025-11-24 01:19:01 +0100

    Fix: Use simple WebView without deprecated APIs

--- app/src/main/java/com/zmr/app/MainActivity.java
+++ app/src/main/java/com/zmr/app/MainActivity.java
@@ -1,36 +1,32 @@
 package com.zmr.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.webview);
+        WebView webView = new WebView(this);
         WebSettings webSettings = webView.getSettings();
-
+        
         // Activer JavaScript
         webSettings.setJavaScriptEnabled(true);
-
-        // Activer la persistance du localStorage
+        
+        // Activer la persistance du localStorage (API moderne)
         webSettings.setDomStorageEnabled(true);
         webSettings.setDatabaseEnabled(true);
-        webSettings.setAppCacheEnabled(true);
-
-        // Spécifier un dossier pour le cache
-        webSettings.setAppCachePath(getApplicationContext().getCacheDir().getPath());
-
+        
         // Empêcher l'ouverture dans le navigateur externe
         webView.setWebViewClient(new WebViewClient());
-
+        
         // Charger votre HTML
         webView.loadUrl("file:///android_asset/index.html");
+        
+        setContentView(webView);
     }
 }
