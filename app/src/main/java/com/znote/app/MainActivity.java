6bf7d4466a117292d8f817afce0fde97a2052461
ihebtmasek-bit <ihebtmasek@gmail.com>
2025-11-25 02:53:57 +0100

    Change app to Z-Note: update package to com.znote.app and app name

---
+++ app/src/main/java/com/znote/app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.znote.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/calculator.html");
+        
+        setContentView(webView);
+    }
+}
